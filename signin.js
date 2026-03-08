b39793860dbdf368889d67da19239891e3f479a0
azidhossainfahim25 <sazidhossainfahim25@gmail.com>
2026-03-08 22:43:02 +0600

    html login page some correction

--- signin.js
+++ signin.js
@@ -1,17 +1,15 @@
 /* sign in js code start here */
 const loginBtn = document.getElementById("login-btn");
 
-loginBtn.addEventListener("click", function(){
+loginBtn.addEventListener("click", function () {
+  const username = document.getElementById("input-name").value;
+  const password = document.getElementById("input-pin").value;
 
-const username = document.getElementById("input-name").value;
-const password = document.getElementById("input-pin").value;
-
-if(username === "admin" && password === "admin123"){
+  if (username === "admin" && password === "admin123") {
     alert("Login Successful");
     window.location.href = "home.html";
-}
-else{
+  } else {
     alert("Wrong Username or Password");
-}
+  }
 });
 /* sign in js code end here */
